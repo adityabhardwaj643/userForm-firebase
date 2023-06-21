edaf326d59de3f7db0488cdb7384028fd643de05
Aditya Bhardwaj <114653579+adityabhardwaj643@users.noreply.github.com>
2023-06-21 13:22:30 +0530

    all tasks done

--- src/App.js
+++ src/App.js
@@ -2,99 +2,129 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import Auth from './components/Auth';
 import { db } from './firebase';
-import { getDocs ,collection,addDoc,deleteDoc,doc} from 'firebase/firestore';//getDocs function gives just the documents but we have to specify from which collection we have to get docs from so we use the collection function and make a instance of it  
-import { deleteUser } from 'firebase/auth';
-//addDoc is used to add a Doc in a  collection
-
+import { getDocs, collection, addDoc, deleteDoc, doc, updateDoc,setDoc } from 'firebase/firestore';
 
 function App() {
+  const [userList, setUserList] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newAge, setNewAge] = useState(0);
+  const [newAddress, setNewAddress] = useState('');
 
-const[userList,setUserList]=useState([])
+  const userCollectionRef = collection(db, 'users');
 
-//New User States
-const[newName,setNewName]=useState("")
-const[newAge,SetNewAge]=useState(0)
-const[newAddress,SetNewAddress]=useState("")
+  const getUserList = async () => {
+    try {
+      const data = await getDocs(userCollectionRef);
+      const filteredData = data.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      console.log(filteredData);
+      setUserList(filteredData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
+  useEffect(() => {
+    getUserList();
+  }, []);
 
-
-
-const userCollectionRef=collection(db,"users") //here we made a reference called userCollectionRef that points to the collection users in database db ,db is the reference of firestore database we made in firebase.js file
-
-
-//get
-const getUserlist=async()=>{
-  //read the data from data list 
-  try{
-  const data = await getDocs(userCollectionRef)
-  const filteredData = data.docs.map((doc)=>(
-    {...doc.data(),
-    id:doc.id,}))
-  console.log(filteredData)
-  setUserList(filteredData)
-  }catch(err){
-    console.error(err)
-  }
-}
-
-useEffect(()=>{
-  getUserlist();
-},[])//we displayed the data below in return
-
-
-//post
-const onSubmitUser=async()=>{
-  try{
-  await addDoc(userCollectionRef,{ //here we add an doc to collection using addDoc method that takes two inputs,1 the ref of the collection in which the doc will be added and the other is the format what should be added 
-    name: newName,
-    age:newAge,
-    Address:newAddress,
-  });
-  getUserlist();
-}catch(err){
-  alert(err)
-  console.error(err)
-}
-
-//delete
-const deleteUser=async(id)=>{
+  const onSubmitUser = async () => {
+    try {
+      const userData = {
+        name: newName,
+        age: newAge,
+        address: newAddress,
+      };
   
-    const userDoc = doc(db,"users",id);
-    await deleteDoc(userDoc)
+      // Get the current user count to determine the next ID
+      const userCountSnapshot = await getDocs(collection(db, 'userCount'));
+      const userCount = userCountSnapshot.docs[0]?.data()?.count || 0;
+      const nextId = String(userCount + 1).padStart(4, '0'); // Pad with leading zeros
   
-}
+      // Create a new document with the generated ID
+      const userDocRef = doc(db, 'users', nextId);
+      await setDoc(userDocRef, userData);
+  
+      // Update the user count
+      const countDocRef = doc(db, 'userCount', 'count');
+      await setDoc(countDocRef, { count: userCount + 1 });
+  
+      getUserList();
+    } catch (err) {
+      alert(err);
+      console.error(err);
+    }
+  };
+  
+  
 
-}
+  const deleteUser = async (id) => {
+    const userDoc = doc(db, 'users', id);
+    await deleteDoc(userDoc);
+    getUserList();
+  };
+
+  const updateUser = async (id) => {
+    const userDoc = doc(db, 'users', id);
+
+    const updatedName = prompt('Enter the new name:');
+    const updatedAge = Number(prompt('Enter the new age:'));
+    const updatedAddress = prompt('Enter the new address:');
+
+    if (updatedName && updatedAge && updatedAddress) {
+      await updateDoc(userDoc, {
+        name: updatedName,
+        age: updatedAge,
+        address: updatedAddress,
+      });
+      getUserList();
+    }
+  };
 
   return (
-    <div className='App' >
+    <div className='App'>
       <Auth></Auth>
 
       <div>
         <label>Enter details of user </label>
-        <input onChange={(e)=>setNewName(e.target.value)} className="mx-2 my-2" placeholder='Enter the name of user'/>
-        <input onChange={(e)=>SetNewAge(Number(e.target.value))} className="mx-2 my-2" placeholder='Enter the age of user' type='number'/>
-        <input onChange={(e)=>SetNewAddress(e.target.value)} className="mx-2 my-2" placeholder='Enter the Address of user'/>
+        <input
+          onChange={(e) => setNewName(e.target.value)}
+          className='mx-2 my-2'
+          placeholder='Enter the name of user'
+        />
+        <input
+          onChange={(e) => setNewAge(Number(e.target.value))}
+          className='mx-2 my-2'
+          placeholder='Enter the age of user'
+          type='number'
+        />
+        <input
+          onChange={(e) => setNewAddress(e.target.value)}
+          className='mx-2 my-2'
+          placeholder='Enter the Address of user'
+        />
         <button onClick={onSubmitUser}>Submit User</button>
-        </div>
-
-
-     <div className='my-5'>
-     <h1>List and Details of Users</h1>
-     
-      <div className='my-5'>
-        {userList.map((user)=>(
-          <div> 
-            <h2>{user.name}</h2>
-            <p>Age:{user.age}</p>
-            <p>Address:{user.Address}</p>
-        </div>
-          
-        ))}
       </div>
+
+      <div className='my-5'>
+        <h1>List and Details of Users</h1>
+
+        <div className='my-5'>
+          {userList.map((user) => (
+            <div key={user.id}>
+              <h2>{user.name}</h2>
+              <p>Age: {user.age}</p>
+              <p>Address: {user.address}</p>
+              <button onClick={() => deleteUser(user.id)}>Delete</button>
+              <button onClick={() => updateUser(user.id)}>Update</button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 }
 
-export default App;
+export default App;
